618fb43d16a9dee3bce07a6ad82797689e601430
Schaback <github@fwdmail.cc>
2021-10-21 15:06:35 +0200

    feat: generate grammar

---
+++ src/main/java/compiler/Parser.java
@@ -0,0 +1,16 @@
+package compiler;
+
+import compiler.ast.AstNode;
+
+public class Parser {
+
+    private Lexer lexer;
+
+    public Parser(Lexer lexer) {
+        this.lexer = lexer;
+    }
+
+    public AstNode parse() {
+        return null;
+    }
+}
