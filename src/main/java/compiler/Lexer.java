f0740f7399cf67f0578a5039eb44d024aaa4bb03
mhuisi <mhuisi@protonmail.com>
2021-10-25 11:03:06 +0200

    feat: lexer primitives

--- src/main/java/compiler/Lexer.java
+++ src/main/java/compiler/Lexer.java
@@ -2,10 +2,97 @@ package compiler;
 
 public class Lexer {
 
+    // Used to signal EOF by peek() and next(). Bubbles up to nextToken(),
+    // where the EOF token is emitted. This saves us the effort
+    // of explicitly handling and forwarding EOF at every layer of the lexer.
+    private static class EOF extends Exception { }
+
     private String fileContent;
+    private int currentPos = 0;
 
     public Lexer(String fileContent) {
         this.fileContent = fileContent;
+    }
+
+    private boolean isEOFAt(int pos) {
+        return pos >= fileContent.length();
+    }
+
+    private boolean isEOF() {
+        return isEOFAt(currentPos);
+    }
+
+    private void checkEOF() throws EOF {
+        if (isEOF()) {
+            throw new EOF();
+        }
+    }
+
+    private char peekAt(int i) {
+        return fileContent.charAt(i);
+    }
+
+    private char peek() {
+        return peekAt(currentPos);
+    }
+
+    private String lookahead(int n) {
+        String result = "";
+        for (int i = currentPos; i < currentPos+n; i++) {
+            if (isEOFAt(i)) {
+                return result;
+            }
+            result += peekAt(i);
+        }
+        return result;
+    }
+
+    private void next(int n) {
+        currentPos += n;
+    }
+
+    private void next() {
+        next(1);
+    }
+
+    private boolean expected(String expected) {
+        String actual = lookahead(expected.length());
+        return actual.equals(expected);
+    }
+
+    private void consumeWhitespace() {
+        while (!isEOF() && Character.isWhitespace(peek())) {
+            next();
+        }
+    }
+
+    private boolean consumeComment() {
+        if (!expected("/*")) {
+            return false;
+        }
+        next(2);
+        while (!isEOF() && !expected("*/")) {
+            next();
+        }
+        // isEOF() = true => no end of comment symbol was found
+        if (isEOF()) {
+            return true;
+        }
+        next(2);
+        return false;
+    }
+
+    private boolean consumeCommentsAndWhitespace() {
+        int lastCurrentPos;
+        do {
+            lastCurrentPos = currentPos;
+            boolean error = consumeComment();
+            if (error) {
+                return true;
+            }
+            consumeWhitespace();
+        } while (lastCurrentPos != currentPos);
+        return false;
     }
 
     /**
@@ -13,6 +100,13 @@ public class Lexer {
      * @return The next token.
      */
     public Token nextToken() {
+        boolean error = consumeCommentsAndWhitespace();
+        if (error) {
+            // TODO: emit error token
+        }
+        if (isEOF()) {
+            // TODO: emit EOF token
+        }
         return null;
     }
 
--- src/main/java/compiler/Token.java
+++ src/main/java/compiler/Token.java
@@ -2,28 +2,60 @@ package compiler;
 
 public class Token {
 
-    private TokenType type;
-
+    public TokenType type;
     private Content content;
-
     public Span span;
 
-    private sealed class Content permits IdentifierContent, IntLiteralContent, ErrorContent {}
+    private Token(TokenType type, Content content, Span span) {
+        this.type = type;
+        this.content = content;
+        this.span = span;
+    }
 
-    private final class IdentifierContent extends Content {
+    public static Token identifier(String name, Span span) {
+        return new Token(TokenType.Identifier, new IdentifierContent(name), span);
+    }
+
+    public static Token intLiteral(int value, Span span) {
+        return new Token(TokenType.IntLiteral, new IntLiteralContent(value), span);
+    }
+
+    public static Token error(String errorMessage, Span span) {
+        return new Token(TokenType.Error, new ErrorContent(errorMessage), span);
+    }
+
+    public static Token eof(Span span) {
+        return new Token(TokenType.EOF, null, span);
+    }
+
+    public static Token keyword(TokenType type, Span span) {
+        return new Token(type, null, span);
+    }
+
+    public static Token operator(TokenType type, Span span) {
+        return new Token(type, null, span);
+    }
+
+    private static sealed class Content permits IdentifierContent, IntLiteralContent, ErrorContent {}
+
+    private static final class IdentifierContent extends Content {
         private String content;
+        private IdentifierContent(String content) { this.content = content; }
     }
 
-    private final class IntLiteralContent extends Content {
+    private static final class IntLiteralContent extends Content {
         private int content;
+        private IntLiteralContent(int content) { this.content = content; }
     }
 
-    private final class ErrorContent extends Content {
+    private static final class ErrorContent extends Content {
         private String errorMessage;
+        private ErrorContent(String errorMessage) { this.errorMessage = errorMessage; }
     }
 
     public String getIdentContent() {
         assert this.type == TokenType.Identifier;
+        assert this.content != null;
         assert this.content instanceof IdentifierContent;
 
         return ((IdentifierContent) this.content).content;
@@ -31,6 +63,7 @@ public class Token {
 
     public int getIntLiteralContent() {
         assert this.type == TokenType.IntLiteral;
+        assert this.content != null;
         assert this.content instanceof IntLiteralContent;
 
         return ((IntLiteralContent) this.content).content;
@@ -38,6 +71,7 @@ public class Token {
 
     public String getErrorContent() {
         assert this.type == TokenType.Error;
+        assert this.content != null;
         assert this.content instanceof ErrorContent;
 
         return ((ErrorContent) this.content).errorMessage;
