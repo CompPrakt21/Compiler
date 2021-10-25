a95a3ae82cb40931ec3cde8b1eb1c98ea14115b3
mhuisi <mhuisi@protonmail.com>
2021-10-25 19:06:48 +0200

    feat: first untested version of lexer

--- src/main/java/compiler/Lexer.java
+++ src/main/java/compiler/Lexer.java
@@ -1,13 +1,32 @@
 package compiler;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Lexer {
 
-    // Used to signal EOF by peek() and next(). Bubbles up to nextToken(),
-    // where the EOF token is emitted. This saves us the effort
-    // of explicitly handling and forwarding EOF at every layer of the lexer.
-    private static class EOF extends Exception { }
+    private static final Map<String, TokenType> KEYWORDS_BY_REPR =
+            TokenType.KEYWORDS.stream().collect(Collectors.toMap(k -> k.repr, k -> k));
 
-    private String fileContent;
+    // Indexes all operators in two ways:
+    // First by length, and then for each length by the concrete string repr.
+    // This index allows for quickly matching the longest operator.
+    private static final List<Map<String, TokenType>> INDEXED_OPERATORS;
+    static {
+        Map<Integer, Map<String, TokenType>> operatorsByLength = TokenType.OPERATORS.stream()
+                .collect(Collectors.groupingBy(o -> o.repr.length(),
+                        Collectors.toMap(o -> o.repr, o -> o)));
+        int maxOperatorLength = operatorsByLength.keySet().stream().max(Integer::compareTo).get();
+        INDEXED_OPERATORS = new ArrayList<>();
+        for (int i = 0; i < maxOperatorLength; i++) {
+            Map<String, TokenType> ops = operatorsByLength.containsKey(i)
+                    ? operatorsByLength.get(i)
+                    : new HashMap<>();
+            INDEXED_OPERATORS.add(ops);
+        }
+    }
+
+    private final String fileContent;
     private int currentPos = 0;
 
     public Lexer(String fileContent) {
@@ -22,12 +41,6 @@ public class Lexer {
         return isEOFAt(currentPos);
     }
 
-    private void checkEOF() throws EOF {
-        if (isEOF()) {
-            throw new EOF();
-        }
-    }
-
     private char peekAt(int i) {
         return fileContent.charAt(i);
     }
@@ -37,14 +50,14 @@ public class Lexer {
     }
 
     private String lookahead(int n) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (int i = currentPos; i < currentPos+n; i++) {
             if (isEOFAt(i)) {
-                return result;
+                return result.toString();
             }
-            result += peekAt(i);
+            result.append(peekAt(i));
         }
-        return result;
+        return result.toString();
     }
 
     private void next(int n) {
@@ -66,33 +79,80 @@ public class Lexer {
         }
     }
 
-    private boolean consumeComment() {
+    private Optional<Span> consumeComment() {
         if (!expected("/*")) {
-            return false;
+            return Optional.empty();
         }
+        int startPos = currentPos;
         next(2);
         while (!isEOF() && !expected("*/")) {
             next();
         }
         // isEOF() = true => no end of comment symbol was found
         if (isEOF()) {
-            return true;
+            return Optional.of(new Span(startPos, currentPos-startPos));
         }
         next(2);
-        return false;
+        return Optional.empty();
     }
 
-    private boolean consumeCommentsAndWhitespace() {
+    private Optional<Span> consumeCommentsAndWhitespace() {
         int lastCurrentPos;
         do {
             lastCurrentPos = currentPos;
-            boolean error = consumeComment();
-            if (error) {
-                return true;
+            Optional<Span> error = consumeComment();
+            if (error.isPresent()) {
+                return error;
             }
             consumeWhitespace();
         } while (lastCurrentPos != currentPos);
-        return false;
+        return Optional.empty();
+    }
+
+    private Optional<Token> consumeAlphanumericWord() {
+        int startPos = currentPos;
+        StringBuilder wordBuilder = new StringBuilder();
+        while (!isEOF() && (Character.isAlphabetic(peek()) || Character.isDigit(peek()))) {
+            wordBuilder.append(peek());
+            next();
+        }
+        String word = wordBuilder.toString();
+        if (word.length() == 0) {
+            return Optional.empty();
+        }
+        Span span = new Span(startPos, currentPos-startPos);
+        if (KEYWORDS_BY_REPR.containsKey(word)) {
+            return Optional.of(Token.keyword(KEYWORDS_BY_REPR.get(word), span));
+        }
+        if (word.chars().allMatch(Character::isDigit)) {
+            return Optional.of(Token.intLiteral(Integer.parseInt(word), span));
+        }
+        if (Character.isAlphabetic(word.charAt(0))) {
+            return Optional.of(Token.identifier(word, span));
+        }
+        return Optional.of(Token.error("Invalid identifier name. First character needs to be alphabetic.", span));
+    }
+
+    private Token consumeOperator() {
+        // We needn't worry about the start of comment symbol /* here.
+        // consumeOperator() is always called when it's guaranteed that
+        // at the current position there's neither whitespace nor a comment,
+        // so the start of comment symbol certainly doesn't occur at currentPos.
+        // OTOH, we will never accidentally match e.g. the beginning of /* (i.e. /)
+        // as part of an operator, because there are no operators with / after the
+        // first character.
+        // If we were to extend the language with more operators, this part of the lexer
+        // might need adjustment.
+        for (int i = INDEXED_OPERATORS.size()-1; i >= 0; i--) {
+            Map<String, TokenType> ops = INDEXED_OPERATORS.get(i);
+            String code = lookahead(i);
+            if (ops.containsKey(code)) {
+                Token result = Token.operator(ops.get(code), new Span(currentPos, i));
+                next(i);
+                return result;
+            }
+        }
+        return Token.error("Invalid symbol.", new Span(currentPos, 1));
     }
 
     /**
@@ -100,14 +160,18 @@ public class Lexer {
      * @return The next token.
      */
     public Token nextToken() {
-        boolean error = consumeCommentsAndWhitespace();
-        if (error) {
-            // TODO: emit error token
+        Optional<Span> error = consumeCommentsAndWhitespace();
+        if (error.isPresent()) {
+            return Token.error("Missing closing `*/` for comment.", error.get());
         }
         if (isEOF()) {
-            // TODO: emit EOF token
+            return Token.eof(new Span(currentPos, 1));
         }
-        return null;
+        Optional<Token> token = consumeAlphanumericWord();
+        if (token.isPresent()) {
+            return token.get();
+        }
+        return consumeOperator();
     }
 
     /**
@@ -115,6 +179,10 @@ public class Lexer {
      * @return The next token.
      */
     public Token peekToken() {
-        return null;
+        int startPos = currentPos;
+        Token next = nextToken();
+        // i wish all side effects allowed for time travel
+        currentPos = startPos;
+        return next;
     }
 }
