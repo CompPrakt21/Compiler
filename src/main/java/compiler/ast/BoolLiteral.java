9d9c1f4d1cdb55f25e044f6857749d7cf7ce184f
uievq <erik.kristiansen@student.kit.edu>
2021-10-26 12:00:14 +0200

    fix: added the Astnode methods

--- src/main/java/compiler/ast/BoolLiteral.java
+++ src/main/java/compiler/ast/BoolLiteral.java
@@ -1,9 +1,21 @@
 package compiler.ast;
 
+import java.util.List;
+
 public final class BoolLiteral extends Expression {
     private boolean value;
 
     public BoolLiteral(boolean value) {
         this.value = value;
+    }
+
+    @Override
+    public List<AstNode> getChildren() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return String.valueOf(value);
     }
 }
--- src/main/java/compiler/ast/IntLiteral.java
+++ src/main/java/compiler/ast/IntLiteral.java
@@ -1,9 +1,21 @@
 package compiler.ast;
 
+import java.util.List;
+
 public final class IntLiteral extends Expression {
     private int value;
 
     public IntLiteral(int value) {
         this.value = value;
+    }
+
+    @Override
+    public List<AstNode> getChildren() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return String.valueOf(value);
     }
 }
