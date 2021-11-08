43e1f6ef195c7314c8afa20aec95074b5eb08c33
neuring <neuring@pm.me>
2021-11-08 16:35:29 +0100

    feat: add AstData type

---
+++ src/main/java/compiler/AstData.java
@@ -0,0 +1,38 @@
+package compiler;
+
+import compiler.ast.AstNode;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class AstData<T> {
+    private ArrayList<T> data;
+
+    public AstData() {
+        this.data = new ArrayList<>();
+    }
+
+    public void set(AstNode a, T data) {
+        var id = a.getID();
+
+        extendDataList(id + 1);
+
+        this.data.set(id, data);
+    }
+
+    public Optional<T> get(AstNode a) {
+        var id = a.getID();
+
+        if (id < data.size()) {
+            return Optional.ofNullable(this.data.get(id));
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    private void extendDataList(int length) {
+        while (this.data.size() < length) {
+            this.data.add(null);
+        }
+    }
+}
