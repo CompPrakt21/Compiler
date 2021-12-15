3556e130a281e916b476f2faa021b2e568e553c8
neuring <neuring@pm.me>
2021-12-15 12:09:59 +0100

    feat: lower not node to llir

--- src/main/java/compiler/codegen/llir/BranchInstruction.java
+++ src/main/java/compiler/codegen/llir/BranchInstruction.java
@@ -7,12 +7,23 @@ import java.util.stream.Stream;
 public final class BranchInstruction extends ControlFlowNode {
 
     public enum Predicate {
-        GREATER_THAN("gt"), GREATER_EQUAL("ge"), LESS_THAN("lt"), LESS_EQUAL("le"), EQUAL("eq");
+        GREATER_THAN("gt"), GREATER_EQUAL("ge"), LESS_THAN("lt"), LESS_EQUAL("le"), EQUAL("eq"), NOT_EQUAL("ne");
 
         private String suffix;
 
         Predicate(String suffix) {
             this.suffix = suffix;
+        }
+
+        public Predicate invert() {
+            return switch (this) {
+                case GREATER_THAN -> LESS_EQUAL;
+                case GREATER_EQUAL -> LESS_THAN;
+                case LESS_THAN -> GREATER_EQUAL;
+                case LESS_EQUAL -> GREATER_THAN;
+                case EQUAL -> NOT_EQUAL;
+                case NOT_EQUAL -> EQUAL;
+            };
         }
 
         public String getSuffix() {
