176ac1c0ad50d258768226ee4637bae9564519c2
Schaback <github@fwdmail.cc>
2022-01-24 16:56:30 +0100

    inlining

--- src/main/java/compiler/InliningOptimization.java
+++ src/main/java/compiler/InliningOptimization.java
@@ -4,7 +4,6 @@ import firm.*;
 import firm.nodes.*;
 
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 public class InliningOptimization {
@@ -13,184 +12,231 @@ public class InliningOptimization {
 
     private Graph graph;
     private ArrayDeque<Node> worklist = new ArrayDeque<>();
-    private DataFlow dataFlow = new DataFlow();
-    private Node predsMemoryCall = null; //needed
-    private Node succsMemoryCall = null; //replaced
-    private ArrayDeque<Node> parameters = new ArrayDeque<>(); //needed
-    private Address addressNode = null; //replaced
-    private Graph addressGraph = null;  //needed
-    private Pair<Node, Integer> isNode = null; //replaced
-    private Node callingProjIs = null; //needed
+
+    private ArrayList<Graph> activatedGraphs = new ArrayList<>();
+    private final int MAX_COPY_SIZE = 25;
+
+
+
+    private int size = Integer.MAX_VALUE;
 
 
     private ArrayList<Call> callNodes = new ArrayList<>();
     private ArrayList<Proj> projNodes = new ArrayList<>();
-    private ArrayList<Pair<Call, Call>> AddressCallAndClassInstantiation = new ArrayList();
 
 
     public InliningOptimization(Graph g) {
         graph = g;
-        BackEdges.enable(graph);
+        if (!BackEdges.enabled(graph))
+            BackEdges.enable(graph);
+        activatedGraphs.add(graph);
         NodeCollector c = new NodeCollector(worklist);
         graph.walkTopological(c);
+        size = worklist.size();
+    }
+
+    public int getSize() {
+        return size;
     }
 
     private void findAllAddressCalls(ArrayDeque<Node> worklist) {
 
         worklist.stream().filter(node -> node instanceof Proj proj && proj.getMode().equals(Mode.getIs())).forEach(node -> projNodes.add((Proj) node));
         worklist.stream().filter(node -> node instanceof Call).forEach(node -> callNodes.add((Call) node));
+        ArrayList<Node> toRemove = new ArrayList<>();
         for (Call callNode : callNodes) {
             ArrayList<Node> temp = new ArrayList<>();
             callNode.getPreds().forEach(node -> temp.add(node));
-            if (temp.stream().noneMatch(node -> node instanceof Address address && address.getEntity().getName().matches("(_System_out_(write|println|read|flush))")))
-                callNodes.remove(temp);
+            if (temp.stream().anyMatch(node -> node instanceof Address address && address.getEntity().getName().matches("(_System_(out|in)_(write|println|read|flush))|__builtin_alloc_function__"))) {
+                toRemove.add(callNode);
+            }
         }
+        callNodes.removeAll(toRemove);
+        suitabilityFilter();
 
     }
 
-    private void suitabilityFilter(ArrayList<Proj> addressNodes) {
-        addressNodes.stream().filter(proj -> proj.getPred() instanceof Proj proj1 && callNodes.contains(proj1.getPred().getPreds()));
+    private void suitabilityFilter() {
+        ArrayList<Proj> projTemp = new ArrayList<>();
+        projNodes.stream().filter(proj -> proj.getPred() instanceof Proj proj1 && callNodes.contains(proj1.getPred())).forEach(projTemp::add);
+        this.projNodes = projTemp;
 
+    }
+
+    private Pair<Node, Proj> copyIntoGraph(ArrayList<Node> listOfToBeCopied, Block targetBlock, Pair<Node, Proj> returns) {
+        Node returnNode;
+        Proj returnProj;
+
+
+        Graph graph = targetBlock.getGraph();
+        HashMap<Node, Node> copied = new HashMap<>();
+        for (Node node : listOfToBeCopied) {
+            Node temp = node.copyInto(graph);
+            temp.setBlock(targetBlock);
+            copied.put(node, temp);
+        }
+
+        Node[] set = new Node[copied.size()];
+        copied.values().toArray(set);
+        for (int i = 0; i < set.length; i++) {
+            Iterator<Node> te = set[i].getPreds().iterator();
+            Node next;
+            int j = 0;
+            while(te.hasNext()) {
+                next = te.next();
+                if (copied.containsKey(next))
+                    set[i].setPred(j, copied.get(next));
+                j++;
+            }
+        }
+        returnNode = copied.get(returns.first);
+        returnProj = (Proj) copied.get(returns.second);
+
+        return new Pair<>(returnNode, returnProj);
+    }
+
+    private void inline(Optional<Proj> resultNode, Node aboveMemoryNodeBeforeAddressCall, Proj belowMemoryNodeAfterAddressCall, ArrayList<Node> sourceParameters, ArrayDeque<Node> remoteParameters, Node remoteResultNode, Proj remoteLastMemoryNode, Proj remoteFirstMemoryNode, Node callNode ) {
+
+        Iterator<Node> predsIterator = callNode.getPreds().iterator();
+         for (int i = 0; i < callNode.getPredCount(); i++) {
+            Node node = predsIterator.next();
+            if (resultNode.isPresent() && node.equals(resultNode.get())) {
+                callNode.setPred(i, remoteResultNode);
+            } else if  (node.equals(belowMemoryNodeAfterAddressCall)) {
+                callNode.setPred(i, remoteLastMemoryNode);
+            }
+        }
+        remoteFirstMemoryNode.setPred(aboveMemoryNodeBeforeAddressCall);
+        ArrayList<Node> parameterChildren = new ArrayList<>();
+        remoteParameters.stream()
+                .forEach(node -> BackEdges.getOuts(node).forEach(edge -> {
+                    if (!parameterChildren.contains(edge.node))
+                        parameterChildren.add(edge.node);
+                }));
+        for (Node parameterChild : parameterChildren) {
+            Iterator<Node> preds = parameterChild.getPreds().iterator();
+            int i = 0;
+            Node pred;
+            while (preds.hasNext()) {
+                pred = preds.next();
+                if (remoteParameters.contains(pred) && parameterChild.getGraph().equals(sourceParameters.get(i).getGraph()))
+                    parameterChild.setPred(i, sourceParameters.get(i));
+                i++;
+            }
+        }
     }
 
     public void collectNodes() {
 
+        findAllAddressCalls(worklist);
 
-        ArrayDeque<Node> addresslist = new ArrayDeque<>(worklist.stream()
-                .filter(((node -> (node instanceof Address) && !(((Address) node).getEntity().getName().matches("_System_out_(write|println|read|flush)")) && !(((Address) node).getEntity().getName().matches("__builtin_alloc_function__")) && node.getMode().equals(Mode.getP()))))
-                        .collect(Collectors.toList()));
+        for (Call callNode : callNodes) {
+            ArrayList<Node> temp = new ArrayList<>();
+            Optional<Proj> resultNode = null; //can be empty
+            Node aboveMemoryNodeBeforeAddressCall = null;
+            Proj belowMemoryNodeAfterAddressCall = null;
+            ArrayList<Node> sourceParameters = new ArrayList<>();
+            ArrayDeque<Node> remoteParameters = new ArrayDeque<>();
+            Node remoteResultNode = null;
+            Proj remoteLastMemoryNode = null;
+            Proj remoteFirstMemoryNode = null;
+            Proj varStorageNode;
+            Return targetReturn = null;
+            Graph curTargetGraph = null;
+            int curTargetGraphSize = Integer.MAX_VALUE;
 
-        addresslist.forEach(node -> System.out.println(node));
+            callNode.getPreds().forEach(node -> {
+                temp.add(node);
+            });
 
-        if (addresslist.isEmpty()) return;
+            for (Node node : temp) {
+                switch (node) {
+                    case Proj proj -> {
+                        if (proj.getMode().equals(Mode.getM()))
+                            aboveMemoryNodeBeforeAddressCall = proj.getPred();
+                        else if (proj.getMode().equals(Mode.getP()))
+                            varStorageNode = proj;
+                        else
+                            sourceParameters.add(proj);
 
-        addressNode = (Address) addresslist.getFirst();
+                    }
+                    case Address address -> {
+                        curTargetGraph = address.getEntity().getGraph();
+                        if (!BackEdges.enabled(curTargetGraph)) {
+                            BackEdges.enable(curTargetGraph);
+                            activatedGraphs.add(curTargetGraph);
+                        }
+                        /*if (!curTargetGraph.equals(graph)) {
+                            InliningOptimization optimization = new InliningOptimization(curTargetGraph);
+                            optimization.collectNodes();
+                            curTargetGraphSize = optimization.getSize();
+                        }*/
+                        if (curTargetGraphSize > MAX_COPY_SIZE)
+                            break;
 
-        Node callingAddressNode = StreamSupport.stream(BackEdges.getOuts(addressNode).spliterator(),  false).collect(Collectors.toList()).get(0).node;
 
-        Iterator<Node> i = callingAddressNode.getPreds().iterator();
-        while(i.hasNext()) {
-            Node tempNode = i.next();
-            if (tempNode.getMode().equals(Mode.getP()) || tempNode.equals(addressNode)) continue;
-            if (tempNode.getMode().equals(Mode.getM())) {
-                predsMemoryCall = StreamSupport.stream(tempNode.getPreds().spliterator(), false).collect(Collectors.toList()).get(0);
-                continue;
-            }
-            parameters.add(tempNode);
-        }
-
-        Iterator<BackEdges.Edge> t = BackEdges.getOuts(callingAddressNode).iterator();
-        while (t.hasNext()) {
-            BackEdges.Edge temp = t.next();
-            Node tempNode = temp.node;
-            if (tempNode.getMode().equals(Mode.getM())) {
-                succsMemoryCall = tempNode;
-                callingProjIs = StreamSupport.stream(BackEdges.getOuts(succsMemoryCall).spliterator(), false).collect(Collectors.toList()).get(0).node;
-                int count = 0;
-                for (Node pred : callingProjIs.getPreds()) {
-                    if (pred.getMode().equals(Mode.getIs())) isNode = new Pair<Node, Integer>(pred, count);
+                        targetReturn = (Return) curTargetGraph.getEndBlock().getPred(0);
+                        ArrayList<Node> toBeCopied = DFSGraph(targetReturn, remoteParameters);
+                        Pair<Node, Proj> unCopied = new Pair<>(targetReturn.getPred(1), (Proj) targetReturn.getPred(0));
+                        Pair<Node, Proj> copiedItems = copyIntoGraph(toBeCopied, (Block) callNode.getBlock(), unCopied);
+                        remoteResultNode = copiedItems.first;
+                        remoteLastMemoryNode = copiedItems.second;
+                        Node tempNode = remoteLastMemoryNode;
+                        while (tempNode.getPred(0).getGraph().equals(remoteLastMemoryNode.getGraph()) && !(tempNode.getPred(0) instanceof Start)) {
+                            tempNode = tempNode.getPred(0);
+                        }
+                        remoteFirstMemoryNode = (Proj) tempNode;
+                    }
+                    default -> {
+                        sourceParameters.add(node);
+                    }
                 }
             }
+            if (curTargetGraphSize < MAX_COPY_SIZE) {
+                Node belowCallNode = null;
+                for (BackEdges.Edge edge : BackEdges.getOuts(callNode)) {
+                    Node node = edge.node;
+                    if (node instanceof Proj proj && proj.getMode().equals(Mode.getM())) {
+                        belowMemoryNodeAfterAddressCall = proj;
+                        belowCallNode = BackEdges.getOuts(proj).iterator().next().node;
+                    } else if (node instanceof Proj proj && proj.getMode().equals(Mode.getT()))
+                        resultNode = projNodes.stream().filter(proj1 -> proj1.getPred().equals(proj)).findFirst();
+                }
 
+                inline(resultNode, aboveMemoryNodeBeforeAddressCall, belowMemoryNodeAfterAddressCall, sourceParameters, remoteParameters, remoteResultNode, remoteLastMemoryNode, remoteFirstMemoryNode, belowCallNode);
+            }
         }
-        succsMemoryCall = StreamSupport.stream(BackEdges.getOuts(callingAddressNode).spliterator(), false).filter(edge -> edge.node.getMode().equals(Mode.getM())).findFirst().get().node;
-        addressGraph = addressNode.getEntity().getGraph();
 
-        succsMemoryCall.setPred(0, predsMemoryCall);
-
-        ArrayDeque<Node> addressWorklist = new ArrayDeque<>();
-        BackEdges.enable(addressGraph);
-        NodeCollector adc = new NodeCollector(addressWorklist);
-        addressGraph.walkTopological(adc);  //Maybe this is not needed
-
-        //addressWorklist.forEach(node -> System.out.println(node));
-
-        //System.out.println();
-        //BackEdges.getOuts(addressGraph.getStart()).forEach(edge -> System.out.println(edge.node));
-
-        ArrayList<Node> addressParameterNodes = new ArrayList<>();
-        ArrayList<Node> oth = DFSGraph(addressWorklist.stream().filter(node -> node instanceof Start).findFirst().get(), addressParameterNodes);
-
-        System.out.println("Parameter");
-        addressParameterNodes.forEach(node -> System.out.println(node));
-        System.out.println("Needed nodes");
-        oth.forEach(node -> System.out.println(node));
-
-
-        //for (int temp = 0; temp < oth.size(); temp++) {
-          //  oth.set();
-        //}#
-        oth.set(1, oth.get(1).copyInto(graph));
-
-        oth.get(1).setBlock(callingProjIs.getBlock());
-        oth.get(1).setPred(0, parameters.getFirst());
-        oth.get(1).setPred(1, parameters.getLast());
-        System.out.println(oth.get(1));
-        oth.get(1).getPreds().forEach(node -> System.out.println(node));
-        callingProjIs.setPred(2, oth.get(oth.size() - 1));
-
-
-
-
-
-
-
-
-
-
-
-        //GraphWanderer addressGraph = new GraphWanderer(dress.getEntity().getGraph());
-        //if (addressGraph.getAllMatching(node -> node instanceof Proj).size() > 1) //TODO: If the method contains a bigger more complex method, find out if better to leave as method
-          //  return;
-
-        //ArrayDeque<Node> successors = graphWanderer.getSuccesors(dress);
-        /*if (successors.size() != 1) {
-            return; //TODO: This should not be the case, as a method may only be called by a "Call"-Node, I think
-        }
-        Node callNode = successors.getFirst();
-        ArrayDeque<Node> callSuccesors = graphWanderer.getSuccesors(callNode);
-
-
-
-        //Changing the predecessor of the Proj M Node to skip the method call, which should then be removed by GC
-        System.out.println("CallSuccesors2");
-        callSuccesors.forEach(node -> System.out.println(node));*/
-        //callSuccesors.stream().filter(node -> node.getMode().equals(Mode.getM())).forEach(node -> node.setPred(0, nodePredPred.getFirst()));
-        BackEdges.disable(addressGraph);
-        BackEdges.disable(graph);
-    }
-
-    private ArrayDeque<Node> getPred(Node node) {
-        return new ArrayDeque<Node>(StreamSupport.stream(node.getPreds().spliterator(), false).toList());
     }
 
     //TODO: store which nodes are first and last
-    private ArrayList<Node> DFSGraph(Node start, ArrayList<Node> parameters) {
+    private ArrayList<Node> DFSGraph(Node start, ArrayDeque<Node> parameters) {
         ArrayList<Node> result = new ArrayList<>();
-        Node curr = start;
         Stack<Node> stack = new Stack<>();
+        Node curr = null;
+        stack.add(start);
+
         do {
-            Iterator<BackEdges.Edge> i = BackEdges.getOuts(curr).iterator();
-            i.forEachRemaining(edge -> {
-                Node node = edge.node;
+            curr = stack.pop();
+            Iterator<Node> i = curr.getPreds().iterator();
+            i.forEachRemaining(node -> {
                 boolean flag = true;
-                if (node instanceof Proj proj && proj.getMode().equals(Mode.getT()));
-                else if (node instanceof Proj && node.getMode().equals(Mode.getIs())) {
-                    System.out.println("Found parameter");
+                if (node instanceof Proj proj && node.getMode().equals(Mode.getIs())) {
+                    flag = false;
                     parameters.add(node);
                 }
-                else if (node instanceof Return) {
+                else if (node instanceof Start) {
                     flag = false;
                     if (!stack.isEmpty() && !stack.contains(node)) {
                         stack.add(0, node);
                     }
                 }
                 else if (!result.contains(node)) result.add(node);
-                if (flag && !stack.contains(node)) stack.add(node);
-            });
-            curr = stack.pop();
+                if (flag && !stack.contains(node))
+                    stack.add(node);
 
-        } while (!stack.isEmpty() && !(curr instanceof Return));
+            });
+        } while (!stack.isEmpty() && !(curr instanceof Start));
 
         return result;
     }
